60e6fa39df72400a596ed2a426e2012a6920495d
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-12-13 23:04:46 +0300

    add file HomePageTest

---
+++ src/test/java/by/mi/ui/HomePageTest.java
@@ -0,0 +1,51 @@
+package by.mi.ui;
+
+import by.mi.ui.pages.homePage.HomePage;
+import by.mi.ui.pages.homePage.HomePageMessage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class HomePageTest extends BaseTest {
+
+    @Test
+    @DisplayName("Проверка открытия старницы 'Cмартфоны'")
+    public void testCheckOpenPageSmartphone() {
+        HomePage homePage = new HomePage();
+
+        homePage
+                .clickButtonAcceptCookie()
+                .clickHeaderMenuSmartphone();
+
+        Assertions.assertEquals(HomePageMessage.TEXT_PAGE_SMARTPHONES, homePage.getTextPageSmartphone());
+    }
+
+    @Test
+    @DisplayName("Проверка открытия старницы 'Планшеты'")
+    public void testCheckOpenPageTablet() {
+        HomePage homePage = new HomePage();
+
+        homePage
+                .clickButtonAcceptCookie()
+                .clickHeaderMenuTablets();
+
+        Assertions.assertEquals(HomePageMessage.TEXT_PAGE_TABLET, homePage.getTextPageTablets());
+    }
+
+
+    @Test
+    @DisplayName("Добавление заказа в корзину используя поиск страницы")
+    public void testAddOrderToBasketUsingPageSearch() {
+        HomePage homePage = new HomePage();
+
+        homePage
+                .clickButtonAcceptCookie()
+                .enterValueInSearch()
+                .clickButtonSearch()
+                .clickButtonDetails();
+//                .clickButtonAddToBasket()
+//                .clickButtonBasket();
+    }
+
+    // дописать тест добавление заказа в корзину через поиск
+}

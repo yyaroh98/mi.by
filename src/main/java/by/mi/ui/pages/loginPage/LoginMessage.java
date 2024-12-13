4ec2499b5e5452fca6b649ef096e28314f55e430
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-12-13 23:06:02 +0300

    add file LoginMessage

---
+++ src/main/java/by/mi/ui/pages/loginPage/LoginMessage.java
@@ -0,0 +1,7 @@
+package by.mi.ui.pages.loginPage;
+
+public class LoginMessage {
+    public static final String INCORRECT_ENTRY_EMAIL_AND_PASSWORD_MESSAGE = "Некорректный ввод";
+    public static final String YOUR_EMAIL_MESSAGE = "Ваш e-mail";
+    public static final String PASSWORD_MESSAGE = "Пароль";
+}

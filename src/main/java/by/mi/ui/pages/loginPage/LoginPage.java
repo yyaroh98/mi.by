510add19f4ff5fbe202d39d8ce84c2ca06f02399
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-12-13 23:06:17 +0300

    add file LoginPage

---
+++ src/main/java/by/mi/ui/pages/loginPage/LoginPage.java
@@ -0,0 +1,41 @@
+package by.mi.ui.pages.loginPage;
+
+import by.mi.ui.driver.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+    private WebDriver driver;
+
+    public LoginPage() {
+        driver = Driver.getDriver();
+    }
+
+    public LoginPage sendKeysEmail(String email) {
+        driver.findElement(By.xpath(LoginXpath.INPUT_EMAIL)).sendKeys(email);
+        return this;
+    }
+
+    public LoginPage sendKeysPassword(String password) {
+        driver.findElement(By.xpath(LoginXpath.INPUT_PASSWORD)).sendKeys(password);
+        return this;
+    }
+
+    public LoginPage clickButtonEnter() {
+        driver.findElement(By.xpath(LoginXpath.BUTTON_SUBMIT)).click();
+        return this;
+    }
+
+    public String getTextMessageIncorrectEmailAndPassword() {
+        return driver.findElement(By.xpath(LoginXpath.MESSAGE_INCORRECT_ENTRY_EMAIL_AND_PASSWORD)).getText();
+    }
+
+    public String getTextYourEmail() {
+        return driver.findElement(By.xpath(LoginXpath.MESSAGE_EMAIL)).getText();
+    }
+
+    public String getTextPassword() {
+        return driver.findElement(By.xpath(LoginXpath.MESSAGE_PASSWORD)).getText();
+    }
+}

6dec4ca10db453a91581ac3adab8c773d393beae
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-12-13 23:05:24 +0300

    add file User

---
+++ src/main/java/by/mi/domain/User.java
@@ -0,0 +1,24 @@
+package by.mi.domain;
+
+public class User {
+
+    private String email;
+    private String password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}

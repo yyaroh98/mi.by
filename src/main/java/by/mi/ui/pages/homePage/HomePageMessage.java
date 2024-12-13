93d30edc9c6346583637c7ff6b4f73deda0c07f9
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-12-13 23:03:56 +0300

    add file HomePageMessage

---
+++ src/main/java/by/mi/ui/pages/homePage/HomePageMessage.java
@@ -0,0 +1,6 @@
+package by.mi.ui.pages.homePage;
+
+public class HomePageMessage {
+    public static final String TEXT_PAGE_SMARTPHONES = "Смартфоны";
+    public static final String TEXT_PAGE_TABLET = "Планшеты";
+}

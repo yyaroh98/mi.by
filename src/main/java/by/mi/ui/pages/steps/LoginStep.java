38810d3b5e450f468137de0dac034f4fcee3ca4a
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-12-13 23:06:32 +0300

    add file LoginStep

---
+++ src/main/java/by/mi/ui/pages/steps/LoginStep.java
@@ -0,0 +1,24 @@
+package by.mi.ui.pages.steps;
+
+import by.mi.domain.User;
+import by.mi.ui.pages.homePage.HomePage;
+import by.mi.ui.pages.loginPage.LoginPage;
+
+public class LoginStep {
+
+    public void fillLoginFormAndSubmit(String email, String password) {
+        HomePage homePage = new HomePage();
+        LoginPage loginPage = new LoginPage();
+
+        homePage
+                .clickButtonAcceptCookie()
+                .clickButtonLogin()
+                .sendKeysEmail(email)
+                .sendKeysPassword(password)
+                .clickButtonEnter();
+    }
+
+    public void fillLoginFormAndSubmit(User user) {
+        fillLoginFormAndSubmit(user.getEmail(), user.getPassword());
+    }
+}

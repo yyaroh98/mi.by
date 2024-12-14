ef89bc4a2b3f705dcc780e17982324079d7b428a
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-12-14 13:33:45 +0300

    refactor HomePageXpath

--- src/main/java/by/mi/ui/pages/homePage/HomePageXpath.java
+++ src/main/java/by/mi/ui/pages/homePage/HomePageXpath.java
@@ -2,19 +2,19 @@ package by.mi.ui.pages.homePage;
 
 public class HomePageXpath {
     public static final String BUTTON_ACCEPT_COOKIES = "//button[@class='button js-cookie_accept']";
-    public static final String BUTTON_LOGIN = "/html/body/div[3]/header/div[1]/div/div/div/a[1]";
+    public static final String BUTTON_LOGIN = "//div[@class='header__top-nav flex items-center']//a[@class='js-open-modal']";
 
     public static final String BUTTON_BASKET = "//a[@id='bx_basketFKauiI']";
-    public static final String BUTTON_MENU_SMARTPHONE = "//div[@class=\"swiper-slide swiper-slide-active\"]//a[@href='/catalog/smartfony/']";
+    public static final String BUTTON_MENU_SMARTPHONE = "//div[@class='swiper-slide swiper-slide-active']//a[@href='/catalog/smartfony/']";
     public static final String BUTTON_MENU_TABLETS = "//div[@class='swiper-slide swiper-slide-next']//a[@href='/catalog/planshety/']";
     public static final String BUTTON_MENU_SMART_WATCHES_AND_BRACELETS = "//div[@class='swiper-slide']//a[@href='/catalog/umnye_chasy_i_braslety/']";
     public static final String BUTTON_MENU_HEADPHONES_AND_EARPHONES = "//div[@class='swiper-slide swiper-slide-duplicate']//a[@href='/catalog/naushniki_i_garnitury/']";
-    public static final String BUTTON_MENU_HOME_APPLIANCES = "//div[@class=\"swiper-slide\"]//a[@href='/catalog/bytovaya_tekhnika/']";
+    public static final String BUTTON_MENU_HOME_APPLIANCES = "//div[@class='swiper-slide']//a[@href='/catalog/bytovaya_tekhnika/']";
     public static final String BUTTON_MENU_TV = "//div[@class='swiper-slide']//a[@href='/catalog/televizory_tv_pristavki_proektory/']";
     public static final String SEARCH_STRING = "//input[@id='smart-title-search-input']";
     public static final String BUTTON_SEARCH = "//span[@id='smart-title-search_preloader_item']";
     public static final String BUTTON_DETAILS = "//a[@class='add-to-card add-to-card--text']";
-    public static final String BUTTON_ADD_TO_BASKET = "//button[@id=\"bx_117848907_4572_mobile_add_basket_link\"]";
+    public static final String BUTTON_ADD_TO_BASKET = "//button[@id='bx_117848907_4572_mobile_add_basket_link']";
 
 
     public static final String TEXT_PAGE_SMARTPHONE = "//h1[@class='h1--thin '][text()='Смартфоны']";

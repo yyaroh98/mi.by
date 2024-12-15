7536a84fa475a1f4bf79d3a76cd54b8d46b510cf
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-12-15 11:11:35 +0300

    add file SearchStringTest

--- src/test/java/by/mi/ui/SearchStringTest.java
+++ src/test/java/by/mi/ui/SearchStringTest.java
@@ -1,30 +1,43 @@
 package by.mi.ui;
 
 import by.mi.ui.pages.homePage.HomePage;
+import by.mi.ui.pages.searchString.SearchStringMessage;
 import by.mi.ui.pages.searchString.SearchStringPage;
+import by.mi.ui.pages.searchString.SearchStringXpath;
+import by.mi.ui.pages.steps.SearchStep;
 import by.mi.ui.utils.MobilePhone;
+import io.restassured.internal.common.assertion.Assertion;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class SearchStringTest extends BaseTest {
 
     @Test
-    @DisplayName("Добавление заказа в корзину используя поиск страницы")
+    @DisplayName("Добавление 'Смартфон Xiaomi 14 Ultra' в корзину, используя поиск страницы")
     public void testAddOrderToBasketUsingPageSearch() {
 
-        HomePage homePage = new HomePage();
-        homePage.clickButtonAcceptCookie();
+        SearchStep searchStep = new SearchStep();
+        searchStep.addOrderIntoBasket(MobilePhone.MOBILE_XIAOMI_14_ULTRA);
 
         SearchStringPage searchStringPage = new SearchStringPage();
-        searchStringPage
-                .sendKeysSearchMobilePhone("Смартфон Xiaomi 14 Ultra")
-                .clickButtonSearch()
-                .clickButtonDetails();
-
-//                .enterValueInSearch()
-//                .clickButtonSearch()
-//                .clickButtonDetails();
-//                .clickButtonAddToBasket()
-//                .clickButtonBasket();
+        Assertions.assertEquals(SearchStringMessage.SMARTPHONE_XIAOMI_14_ULTRA, searchStringPage.getTextMobileInTheBasket());
     }
 }
+//@Test
+//@DisplayName("Добавление заказа в корзину используя поиск страницы")
+//public void testAddOrderToBasketUsingPageSearch() {
+//
+//    HomePage homePage = new HomePage();
+//    homePage.clickButtonAcceptCookie();
+//
+//    SearchStringPage searchStringPage = new SearchStringPage();
+//    searchStringPage
+//            .sendKeysSearchMobilePhone("Смартфон Xiaomi 14 Ultra")
+//            .clickButtonSearch()
+//            .clickButtonDetails()
+//            .clickButtonAddToBasket()
+//            .clickGoToTheBasket();
+//
+//    Assertions.assertEquals(SearchStringMessage.SMARTPHONE_XIAOMI_14_ULTRA, searchStringPage.getTextMobileInTheBasket());
+//}

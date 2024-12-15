45425223f84d330846d8945184010a72f98ea82f
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-12-15 11:10:41 +0300

    add file SearchStringXpath

--- src/main/java/by/mi/ui/pages/searchString/SearchStringXpath.java
+++ src/main/java/by/mi/ui/pages/searchString/SearchStringXpath.java
@@ -2,7 +2,8 @@ package by.mi.ui.pages.searchString;
 
 public class SearchStringXpath {
     public static final String BUTTON_DETAILS = "//*[@id='bx_3966226736_4572_7e1b8e3524755c391129a9d7e6f2d206_basket_actions']";
-    public static final String BUTTON_DETAILS1 = "//*[@id=\"bx_3966226736_4572_7e1b8e3524755c391129a9d7e6f2d206_pict\"]";
-    public static final String BUTTON_ADD_TO_BASKET = "//button[@id='bx_117848907_4572_mobile_add_basket_link']";
-
+    public static final String BUTTON_DETAILS1 = "//*[@id='bx_3966226736_4572_7e1b8e3524755c391129a9d7e6f2d206_pict']";
+    public static final String BUTTON_ADD_TO_BASKET = "//*[@id='bx_117848907_4572_add_basket_link']";
+    public static final String BUTTON_GO_TO_THE_BASKET = "//span[@class='btn btn-default btn-buy btn-sm']";
+    public static final String NAME_MOBILE_IN_BASKET = "//div[@class='good__info']//a[@href='/catalog/smartfony/xiaomi/smartfon_xiaomi_14_ultra/']";
 }

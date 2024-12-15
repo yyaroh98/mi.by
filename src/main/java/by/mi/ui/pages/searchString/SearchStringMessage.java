65eae0fd6059ba269ffe8d204f57cfc32a766fe5
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-12-15 11:10:17 +0300

    add file SearchStringMessage

--- src/main/java/by/mi/ui/pages/searchString/SearchStringMessage.java
+++ src/main/java/by/mi/ui/pages/searchString/SearchStringMessage.java
@@ -1,4 +1,5 @@
 package by.mi.ui.pages.searchString;
 
 public class SearchStringMessage {
+    public static final String SMARTPHONE_XIAOMI_14_ULTRA = "Смартфон Xiaomi 14 Ultra 16GB/512GB < Black>";
 }

310688e599d059ba380a37e8fd99af52a6ff8b38
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-12-15 11:09:41 +0300

    add file MobilePhone

--- src/main/java/by/mi/ui/utils/MobilePhone.java
+++ src/main/java/by/mi/ui/utils/MobilePhone.java
@@ -1,5 +1,5 @@
 package by.mi.ui.utils;
 
 public class MobilePhone {
-    private static final String MOBILE_XIAOMI_14_ULTRA = "Смартфон Xiaomi 14 Ultra";
+    public static final String MOBILE_XIAOMI_14_ULTRA = "Смартфон Xiaomi 14 Ultra";
 }

ea30fb6e31e8fa9a05d5d47f09d5f239fbefcbbe
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-12-16 18:49:44 +0300

    add file LoginRequest

---
+++ src/test/java/by/mi/api/LoginApiTest.java
@@ -0,0 +1,53 @@
+package by.mi.api;
+
+import by.mi.domain.Users;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class LoginApiTest {
+
+    @BeforeEach
+    void setUp() {
+        LoginRequest.initRequestSpecification();
+    }
+
+    @Test
+    @DisplayName("Авторизация с неверным email и password")
+    public void testWithIncorrectEmailAndPassword() {
+
+        given()
+                .spec(LoginRequest.requestSpecification)
+                .body(LoginRequest.getBody(Users.getUserWithIncorrectEmailAndPassword()))
+                .when()
+                .post()
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("status", equalTo("error"))
+                .body("data", equalTo(null));
+        // .body("message", equalTo("Неверный логин или пароль.<br>"));
+    }
+
+    @Test
+    @DisplayName("Авторизация с верным email и password")
+    public void testWithCorrectEmailAndPassword() {
+        String email = "yaroshzhenya_98@mail.ru";
+        String password = "c412f1809";
+
+        given()
+                .spec(LoginRequest.requestSpecification)
+                .body(LoginRequest.getBody("email", "password"))
+                .when()
+                .post()
+                .then()
+                .log().all()
+                .statusCode(200);
+        //   .body("data", equalTo("true"));
+    }
+
+
+}

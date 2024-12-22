e28c62878fcff83e08a83496fd3407870f8289e6
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-12-22 23:22:46 +0300

    some corrections in the file HomePageXpath

--- src/main/java/by/mi/ui/pages/homePage/HomePageXpath.java
+++ src/main/java/by/mi/ui/pages/homePage/HomePageXpath.java
@@ -3,17 +3,25 @@ package by.mi.ui.pages.homePage;
 public class HomePageXpath {
     public static final String BUTTON_ACCEPT_COOKIES = "//button[@class='button js-cookie_accept']";
     public static final String BUTTON_LOGIN = "//div[@class='header__top-nav flex items-center']//a[@class='js-open-modal']";
+    public static final String BUTTON_CATALOG = "//button[@class='header__catalog base-button j-catalog-btn']";
+    public static final String SEARCH_STRING = "//input[@id='smart-title-search-input']";
+    public static final String BUTTON_SEARCH = "//button[@name='s']";
     public static final String BUTTON_BASKET = "//a[@id='bx_basketFKauiI']";
     public static final String BUTTON_MENU_SMARTPHONE = "//div[@class='swiper-slide swiper-slide-active']//a[@href='/catalog/smartfony/']";
     public static final String BUTTON_MENU_TABLETS = "//div[@class='swiper-slide swiper-slide-next']//a[@href='/catalog/planshety/']";
     public static final String BUTTON_MENU_SMART_WATCHES_AND_BRACELETS = "//div[@class='swiper-slide']//a[@href='/catalog/umnye_chasy_i_braslety/']";
-    public static final String BUTTON_MENU_HEADPHONES_AND_EARPHONES = "//div[@class='swiper-slide swiper-slide-duplicate']//a[@href='/catalog/naushniki_i_garnitury/']";
-    public static final String BUTTON_MENU_HOME_APPLIANCES = "//div[@class='swiper-slide']//a[@href='/catalog/bytovaya_tekhnika/']";
+    public static final String BUTTON_MENU_HEADPHONES_AND_EARPHONES = "//div[@class='swiper-slide']//a[@href='/catalog/naushniki_i_garnitury/']";
     public static final String BUTTON_MENU_TV = "//div[@class='swiper-slide']//a[@href='/catalog/televizory_tv_pristavki_proektory/']";
-    public static final String SEARCH_STRING = "//input[@id='smart-title-search-input']";
-    public static final String BUTTON_SEARCH = "//button[@name='s']";
+    public static final String BUTTON_CATALOG_MENU_HOUSEHOLD_APPLIANCES = "//div[@class='menuCatalog__categories']//a[@href='/catalog/bytovaya_tekhnika/']";
+    public static final String BUTTON_CATALOG_MENU_VACUUM_CLEANERS = "//div[@class='menuCatalog__categories']//a[@href='/catalog/pylesosy/']";
+    public static final String BUTTON_ADD_TO_BASKET_VACUUM_CLEANERS = "//button[@id='bx_3966226736_4558_7e1b8e3524755c391129a9d7e6f2d206_add_to_basket']";
+    public static final String BUTTON_GO_TO_THE_BASKET = "//div[@class='popup-window-buttons']";
 
     public static final String TEXT_PAGE_SMARTPHONE = "//h1[@class='h1--thin '][text()='Смартфоны']";
     public static final String TEXT_PAGE_TABLET = "//h1[@class=\"h1--thin \"][text()='Планшеты']";
+    public static final String TEXT_PAGE_VACUUM_CLEANERS = "//h1[@class=\"h1--thin \"]";
+    public static final String TEXT_PAGE_HOUSEHOLD_APPLIANCES = "//h1[@class=\"h1--thin \"][text()='Бытовая техника']";
     public static final String TEXT_ORDER_IN_THE_BASKET = "//a[@class='good__name']";
+    public static final String TEXT_ORDER_IN_THE_BASKET_VACUUM_CLEANER = "//a[@class='good__name']";
+
 }

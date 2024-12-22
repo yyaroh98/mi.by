f9e5cc3661d0eeaff3a2eee2d82662380445551c
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-12-22 23:21:41 +0300

    some corrections in the file GenerationDataUtil

--- src/main/java/by/mi/ui/utils/GenerationDataUtil.java
+++ src/main/java/by/mi/ui/utils/GenerationDataUtil.java
@@ -2,15 +2,36 @@ package by.mi.ui.utils;
 
 import com.github.javafaker.Faker;
 
+import java.util.Random;
+
 public class GenerationDataUtil {
 
-    public static String generateEmail() {
+    public static String generateValidEmail() {
         Faker faker = new Faker();
         return faker.internet().emailAddress();
     }
 
-    public static String generatePassword() {
+    public static String generateValidPassword() {
         Faker faker = new Faker();
         return faker.internet().password();
+    }
+
+    public static String generateIncorrectEmail() {
+        Faker faker = new Faker();
+        return faker.name().username();
+    }
+
+    public static String generateRandomPassword() {
+        Random random = new Random();
+        int maxLength = 10;
+        int minLength = 4;
+        int length = random.nextInt(maxLength - minLength + 1) + minLength;
+
+        String symbol = "~!@#$%^&*()_=+-|/?;:";
+        char[] symbols = new char[length];
+        for (int i = 0; i < symbols.length; i++) {
+            symbols[i] = symbol.charAt(random.nextInt(symbol.length()));
+        }
+        return new String(symbols);
     }
 }

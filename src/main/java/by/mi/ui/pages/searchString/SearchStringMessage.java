73ad130fd382311f9c14dbdb823b40af9f0d503e
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-12-22 23:24:16 +0300

    some corrections in the file SearchStringMessage

--- src/main/java/by/mi/ui/pages/searchString/SearchStringMessage.java
+++ src/main/java/by/mi/ui/pages/searchString/SearchStringMessage.java
@@ -1,5 +1,6 @@
 package by.mi.ui.pages.searchString;
 
 public class SearchStringMessage {
-    public static final String SMARTPHONE_XIAOMI_14_ULTRA = "Смартфон Xiaomi 14 Ultra 16GB/512GB < Black>";
+    public static final String SMARTPHONE_XIAOMI_MIX_FLIP_IN_THE_BASKET = "Смартфон XIAOMI MIX Flip 12GB/512GB Black RU";
+    public static final String TABLET_XIAOMI_PAD_6S_PRO = "Планшет Xiaomi Pad 6S Pro 12GB/512GB Graphite Gray RU";
 }

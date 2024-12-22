d2f19c1ef02ddc609bf1e9d54f0daac1b7f2d755
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-12-22 23:24:04 +0300

    some corrections in the file SearchStep

--- src/main/java/by/mi/ui/pages/steps/SearchStep.java
+++ src/main/java/by/mi/ui/pages/steps/SearchStep.java
@@ -4,14 +4,26 @@ import by.mi.ui.pages.homePage.HomePage;
 import by.mi.ui.pages.searchString.SearchStringPage;
 
 public class SearchStep {
-    public void addOrderIntoBasket(String mobile) {
+    public void addOrderIntoBasket(String value) {
 
         HomePage homePage = new HomePage();
         homePage.clickButtonAcceptCookie();
 
         SearchStringPage searchStringPage = new SearchStringPage();
         searchStringPage
-                .sendKeysSearchMobilePhone(mobile)
+                .sendKeysSearch(value)
+                .clickButtonSearch()
+                .clickButtonDetails()
+                .clickButtonAddToBasket()
+                .clickGoToTheBasket();
+    }
+
+    public void addTwoOrdersIntoBasket(String value1, String value2) {
+
+        addOrderIntoBasket(value1);
+        SearchStringPage searchStringPage = new SearchStringPage();
+        searchStringPage
+                .sendKeysSearch(value2)
                 .clickButtonSearch()
                 .clickButtonDetails()
                 .clickButtonAddToBasket()

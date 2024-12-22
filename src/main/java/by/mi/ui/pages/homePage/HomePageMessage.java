44e21664495e78213ea45aa421abc7d52735fe45
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-12-22 23:22:16 +0300

    some corrections in the file HomePageMessage

--- src/main/java/by/mi/ui/pages/homePage/HomePageMessage.java
+++ src/main/java/by/mi/ui/pages/homePage/HomePageMessage.java
@@ -3,4 +3,7 @@ package by.mi.ui.pages.homePage;
 public class HomePageMessage {
     public static final String TEXT_PAGE_SMARTPHONES = "Смартфоны";
     public static final String TEXT_PAGE_TABLET = "Планшеты";
+    public static final String TEXT_PAGE_VACUUM_CLEANERS = "Пылесосы";
+    public static final String TEXT_PAGE_HOUSEHOLD_APPLIANCES = "Бытовая техника";
+    public static final String TEXT_PAGE_SMART_WATCHES_AND_BRACELETS = "Умные часы и браслеты";
 }

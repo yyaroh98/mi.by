432ec2dac46c6d5036eb54ada128a4eb672c2823
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-12-23 00:03:47 +0300

    refactor file LoginRequest

--- src/test/java/by/mi/api/LoginApiTest.java
+++ src/test/java/by/mi/api/LoginApiTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class LoginApiTest {
